4e7efa5ddf0310ec72b36d4401fc44bc9c9ef830
Geo Milano <george_limkt@hotmail.com>
2015-01-09 18:01:38 +0800

    updated

---
+++ Resources/iphone/alloy/styles/m_myMedical.js
@@ -0,0 +1,1 @@
+module.exports = [{"isClass":true,"priority":10000.0016,"key":"container","style":{}},{"isId":true,"priority":100000.0001,"key":"activityIndicator","style":{style:Alloy.Globals.topbarTop,top:30,left:30,width:60,}}];
