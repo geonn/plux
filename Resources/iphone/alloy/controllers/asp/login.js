c84265dd9e1d02a633bc63839728c5b8a35f69a4
Geo Milano <george_limkt@hotmail.com>
2015-05-25 21:38:12 +0800

    ignore

--- Resources/iphone/alloy/controllers/asp/login.js
+++ Resources/iphone/alloy/controllers/asp/login.js
@@ -29,10 +29,7 @@ function Controller() {
     }
     require("alloy/controllers/BaseController").apply(this, Array.prototype.slice.call(arguments));
     this.__controllerPath = "asp/login";
-<<<<<<< HEAD
     this.args = arguments[0] || {};
-=======
->>>>>>> origin/master
     if (arguments[0]) {
         {
             __processArg(arguments[0], "__parentSymbol");
--- Resources/iphone/alloy/controllers/myHealth/healthDataBmi.js
+++ Resources/iphone/alloy/controllers/myHealth/healthDataBmi.js
@@ -63,6 +63,7 @@ function Controller() {
     }
     require("alloy/controllers/BaseController").apply(this, Array.prototype.slice.call(arguments));
     this.__controllerPath = "myHealth/healthDataBmi";
+    this.args = arguments[0] || {};
     if (arguments[0]) {
         {
             __processArg(arguments[0], "__parentSymbol");
