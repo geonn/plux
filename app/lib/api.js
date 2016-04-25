c4a644f1f2e7fc2bf688c6b6b35fc7da248aee33
Geo Milano <geomilano@jts-MacBook-Pro.local>
2016-04-25 15:08:18 +0800

    added u_id in update Notification Token

--- app/lib/api.js
+++ app/lib/api.js
@@ -729,9 +729,10 @@ exports.claimInfo = function(e) {
 exports.updateNotificationToken = function(e){
 	var deviceToken = Ti.App.Properties.getString('deviceToken');
 	var memno = Ti.App.Properties.getString('memno'); 
+	var u_id = Ti.App.Properties.getString('u_id') || ""; 
 	if(deviceToken != ""){ 
-		var url = updateToken+"&token="+deviceToken+"&member_no="+memno;
-		//console.log(url);
+		var url = updateToken+"&token="+deviceToken+"&member_no="+memno+"&u_id="+u_id;
+		console.log(url);
 		var client = Ti.Network.createHTTPClient({
 		     // function called when the response data is available
 		     onload : function(e) {
