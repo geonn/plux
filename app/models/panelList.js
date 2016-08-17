b8f5be86de0238a31495d61b829ee160d98e5280
lauyikonn <lau.yikonn@gmail.com>
2016-08-17 16:35:38 +0800

    conflict fixed

--- app/models/panelList.js
+++ app/models/panelList.js
@@ -507,10 +507,7 @@ exports.definition = {
                 }
                 db.execute("BEGIN");
                 arr.forEach(function(entry) {
-<<<<<<< HEAD
-=======
-                	//console.log(entry.id+" "+entry.clinicName);
->>>>>>> origin/master
+
                 	var keys = [];
                 	var questionmark = [];
                 	var eval_values = [];
