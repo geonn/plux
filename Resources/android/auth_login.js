cc08a8876ede19d62df29d5dfe37d7d42f8fc1c7
DESKTOP-APQ55P4\rexmi <rexming1999@gmail.com>
2017-06-05 12:55:04 +0800

    inpatient record row design

---
+++ Resources/android/auth_login.js
@@ -0,0 +1,6 @@
+var common = require("common");
+
+exports.checkLogin = function() {
+    var memno = Ti.App.Properties.getString("memno");
+    return void 0 !== typeof memno && null != memno && "" != memno;
+};
