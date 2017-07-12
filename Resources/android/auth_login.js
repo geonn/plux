58660856c4e866e87178a437df18d9c42f82c681
Geo Milano <geomilano@Geos-MacBook-Pro.local>
2017-07-12 23:44:58 +0800

    ignore

--- Resources/android/auth_login.js
+++
@@ -1,6 +0,0 @@
-var common = require("common");
-
-exports.checkLogin = function() {
-    var memno = Ti.App.Properties.getString("memno");
-    return void 0 !== typeof memno && null != memno && "" != memno;
-};
--- app/lib/AppVersionControl.js
+++ app/lib/AppVersionControl.js
@@ -7,7 +7,7 @@
 
 // update user device token
 exports.checkAndUpdate = function(e){
-	Ti.App.Properties.setString("appVersion", "1.1.988");
+	Ti.App.Properties.setString("appVersion", "1.1.989");
 
 	API.checkAppVersion(callback_download);
 };
