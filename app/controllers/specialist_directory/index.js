87b6775b199c02a84a77ee6576dc35c94a681afc
Lau Yik Onn <lau.yikonn@gmail.com>
2020-09-04 12:31:21 +0800

    added specialist_directory button and page for danial

---
+++ app/controllers/specialist_directory/index.js
@@ -0,0 +1,25 @@
+// Arguments passed into this controller can be accessed via the `$.args` object directly or:
+var args = $.args;
+var loading = Alloy.createController("loading");
+var u_id = Ti.App.Properties.getString('u_id'); 
+
+function init(){
+	$.win.add(loading.getView());
+	loading.start();
+	setTimeout(function(){
+		loading.finish();
+	}, 3000);
+	
+}
+
+init();
+
+if(OS_ANDROID){ 
+	$.btnBack.addEventListener('click', function(){ 
+		$.win.close(); 
+	});
+}
+
+$.win.addEventListener("close", function(){
+	$.destroy();
+});
